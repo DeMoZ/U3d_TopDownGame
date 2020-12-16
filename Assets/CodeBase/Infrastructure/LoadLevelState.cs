7c2467d6123297b67970bef6795ceb979ac6bfb9
Roman Kolychev <demobox@gmail.com>
2020-12-16 16:29:29 +0300

    StateMachine implemented.

---
+++ Assets/CodeBase/Infrastructure/LoadLevelState.cs
@@ -0,0 +1,61 @@
+﻿using CodeBase.CameraLogic;
+using UnityEngine;
+
+namespace CodeBase.Infrastructure
+{
+    public class LoadLevelState : IPayloadedState<string>
+    {
+        private const string HeroPath = "Hero/Hero";
+        private const string HudPath = "Hud/Hud";
+        private const string InitialPoint = "InitialPoint";
+
+        private readonly GameStateMachine _stateMachine;
+        private readonly SceneLoader _sceneLoader;
+        private readonly LoadingCurtain _curtain;
+
+        public LoadLevelState(GameStateMachine stateMachine, SceneLoader sceneLoader, LoadingCurtain curtain)
+        {
+            _sceneLoader = sceneLoader;
+            _curtain = curtain;
+            _stateMachine = stateMachine;
+        }
+
+        public void Enter(string sceneName)
+        {
+            _curtain.Show();
+            _sceneLoader.Load(sceneName, OnLoaded);
+        }
+
+        public void Exit() =>
+            _curtain.Hide();
+
+        private void OnLoaded()
+        {
+            GameObject initialPoint = GameObject.FindWithTag(InitialPoint);
+            GameObject hero = InstantiateObject(HeroPath, initialPoint.transform.position);
+
+            InstantiateObject(HudPath);
+
+            CameraFollow(hero);
+
+            _stateMachine.Enter<GameLoopState>();
+        }
+
+        private void CameraFollow(GameObject follow) =>
+            Camera.main.GetComponent<CameraFollow>().Follow(follow);
+
+        private static GameObject InstantiateObject(string path)
+        {
+            GameObject prefab = Resources.Load<GameObject>(path);
+            return Object.Instantiate(prefab);
+        }
+
+        private static GameObject InstantiateObject(string path, Vector3 at)
+        {
+            GameObject prefab = Resources.Load<GameObject>(path);
+            prefab = Object.Instantiate(prefab, at, Quaternion.identity);
+
+            return prefab;
+        }
+    }
+}
---
+++ Assets/CodeBase/Infrastructure/LoadingCurtain.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using UnityEngine;
+
+namespace CodeBase.Infrastructure
+{
+    [RequireComponent(typeof(CanvasGroup))]
+    public class LoadingCurtain : MonoBehaviour
+    {
+        [SerializeField] private CanvasGroup Curtain;
+
+        private void Awake()
+        {
+            DontDestroyOnLoad(this);
+        }
+
+        public void Show()
+        {
+            gameObject.SetActive(true);
+            Curtain.alpha = 1;
+        }
+
+        public void Hide() =>
+            StartCoroutine(FadeIn());
+
+        private IEnumerator FadeIn()
+        {
+            while (Curtain.alpha > 0)
+            {
+                Curtain.alpha -= 0.3f;
+                yield return new WaitForSeconds(0.3f);
+            }
+
+            gameObject.SetActive(false);
+        }
+    }
+}
