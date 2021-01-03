95e02088939fa18af5e62edaabed1bfb02cbdb0b
Roman Kolychev <demobox@gmail.com>
2021-01-03 15:13:32 +0300

    Started on Player Progress Services

---
+++ Assets/CodeBase/Services/PersistentProgress/PersistantProgressService.cs
@@ -0,0 +1,13 @@
+﻿using CodeBase.Data;
+
+namespace CodeBase.Services.PersistantProgress
+{
+    public class PersistentProgressService : IPersistentProgressService
+    {
+        private PlayerProgress Progress { get; set; }
+    }
+
+    public interface IPersistentProgressService: IService
+    {
+    }
+}
